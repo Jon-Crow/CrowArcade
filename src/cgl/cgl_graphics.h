2939a58b180abdb7e9eac01ee434055a8bdbebcd
jon-crow <cjonat1@vt.edu>
2025-12-28 08:06:49 +0000

    Added doxygen comments to graphics

--- src/cgl/cgl_graphics.h
+++ src/cgl/cgl_graphics.h
@@ -14,9 +14,53 @@
 #include "cgl_animation.h"
 #include "cgl_font.h"
 
+/**
+*@brief Helper function to handle SDL calls necessary for drawing a texture
+*
+*@param ctx Context with relevant SDL handles
+*@param tx  Texture to be drawn
+*@param x   X position of the desired screen location
+*@param y   Y position of the desired screen location
+*@param w   Width of the desired screen location
+*@param h   Height of the desired screen location
+*/
 void CGL_DrawTexture(CGL_Context *ctx, CGL_Texture *tx, int x, int y, int w, int h);
+
+/**
+*@brief Helper function to handle SDL calls necessary for drawing a texture region
+*
+*@param ctx Context with relevant SDL handles
+*@param reg Texture region to be drawn
+*@param x   X position of the desired screen location
+*@param y   Y position of the desired screen location
+*@param w   Width of the desired screen location
+*@param h   Height of the desired screen location
+*/
 void CGL_DrawTextureRegion(CGL_Context *ctx, CGL_TextureRegion *reg, int x, int y, int w, int h);
+
+/**
+*@brief Helper function to handle SDL calls necessary for drawing the current frame of the given animation
+*
+*@param ctx  Context with relevant SDL handles
+*@param anim Animation to be drawn
+*@param x    X position of the desired screen location
+*@param y    Y position of the desired screen location
+*@param w    Width of the desired screen location
+*@param h    Height of the desired screen location
+*/
 void CGL_DrawAnimation(CGL_Context *ctx, CGL_Animation *anim, int x, int y, int w, int h);
+
+/**
+*@brief Helper function to handle SDL calls and position for drawing the glyphs that represent a given string
+*
+*@param ctx   Context with relevant SDL handles
+*@param str   String to be drawn
+*@param x     X position of the desired screen location
+*@param y     Y position of the desired screen location
+*@param charW Width of each character glyph
+*@param charH Height of each character glyph
+*@param clr   The tint to be applied to each glyph. NULL if no tint is to be added
+*/
 void CGL_DrawString(CGL_Context *ctx, CGL_Font *font, const char *str, int x, int y, int charW, int charH, const SDL_Color *clr);
 
 #endif
