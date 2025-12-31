59c6ce7c852655b0989e26518be2cc2dbdff9dd8
jon-crow <cjonat1@vt.edu>
2025-12-31 05:35:11 +0000

    Corrected logger bug with passing va_list as ...

--- src/cgl/cgl_log.c
+++ src/cgl/cgl_log.c
@@ -19,10 +19,10 @@ const char* CGL_LogGetLevelLabel(CGL_LogLevel lvl)
 void CGL_Log(CGL_LogLevel lvl, const char *fmt, ...)
 {
   time_t now = time(NULL);
-  struct tm *tm = localtime(&now);
+  struct tm *time = localtime(&now);
   char timeBuff[64];
 
-  strftime(timeBuff, sizeof(timeBuff), "%H:%M:%S", tm);
+  strftime(timeBuff, sizeof(timeBuff), "%H:%M:%S", time);
 
   printf("%s [%s]: ", CGL_LogGetLevelLabel(lvl), timeBuff);
 
@@ -34,11 +34,24 @@ void CGL_Log(CGL_LogLevel lvl, const char *fmt, ...)
   printf("\n");
 }
 
+void CGL_VLog(CGL_LogLevel lvl, const char *fmt, va_list args)
+{
+  time_t now = time(NULL);
+  struct tm *time = localtime(&now);
+  char timeBuff[64];
+
+  strftime(timeBuff, sizeof(timeBuff), "%H:%M:%S", time);
+
+  printf("%s [%s]: ", CGL_LogGetLevelLabel(lvl), timeBuff);
+  vprintf(fmt, args);
+  printf("\n");
+}
+
 void CGL_LogInfo(const char *fmt, ...)
 {
   va_list args;
   va_start(args, fmt);
-  CGL_Log(CGL_LOG_INFO, fmt, args);
+  CGL_VLog(CGL_LOG_INFO, fmt, args);
   va_end(args);
 }
 
@@ -46,7 +59,7 @@ void CGL_LogWarning(const char *fmt, ...)
 {
   va_list args;
   va_start(args, fmt);
-  CGL_Log(CGL_LOG_WARNING, fmt, args);
+  CGL_VLog(CGL_LOG_WARNING, fmt, args);
   va_end(args);
 }
 
@@ -54,7 +67,7 @@ void CGL_LogError(const char *fmt, ...)
 {
   va_list args;
   va_start(args, fmt);
-  CGL_Log(CGL_LOG_ERROR, fmt, args);
+  CGL_VLog(CGL_LOG_ERROR, fmt, args);
   va_end(args);
 }
 
